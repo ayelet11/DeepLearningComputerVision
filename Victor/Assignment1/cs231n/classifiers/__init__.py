8aa1c9cfc261834d76c126c083bac3a889d58477
victor yampolsky <tsanhan@gmail.com>
2019-02-08 09:49:01 +0200

    asd

--- Victor/Assignment1/cs231n/classifiers/__init__.py
+++ Victor/Assignment1/cs231n/classifiers/__init__.py
@@ -1,2 +1,2 @@
-from cs231n.classifiers.k_nearest_neighbor import *
-from cs231n.classifiers.linear_classifier import *
+from DeepLearningComputerVision.Victor.Assignment1.cs231n.classifiers.k_nearest_neighbor import *
+from DeepLearningComputerVision.Victor.Assignment1.cs231n.classifiers.linear_classifier import *
